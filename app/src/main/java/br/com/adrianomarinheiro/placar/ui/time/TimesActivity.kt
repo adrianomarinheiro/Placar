d9d509c501247352c2b946bd2e5dde6bb71e0a02
Logon Aluno <logonrmlocal@fiap.com.br>
2018-12-10 23:10:51 -0200

    Initial commit

---
+++ app/src/main/java/br/com/adrianomarinheiro/placar/ui/time/TimesActivity.kt
@@ -0,0 +1,31 @@
+package br.com.adrianomarinheiro.placar.ui.time
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.adrianomarinheiro.placar.ui.placar.PlacarActivity
+import br.com.adrianomarinheiro.placar.R
+import br.com.adrianomarinheiro.placar.ui.placaraac.PlacarAACActivity
+import br.com.adrianomarinheiro.placar.utils.KEY_EXTRA_TIME_A
+import br.com.adrianomarinheiro.placar.utils.KEY_EXTRA_TIME_B
+import kotlinx.android.synthetic.main.activity_times.*
+
+class TimesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_times)
+
+        btIniciarJogo.setOnClickListener{
+            val intent = Intent (this, PlacarAACActivity::class.java)
+            intent.putExtra(KEY_EXTRA_TIME_A, inputTimeA.text.toString())
+            intent.putExtra(KEY_EXTRA_TIME_B, inputTimeB.text.toString())
+            startActivity(intent)
+        }
+
+        btNovoJogo.setOnClickListener{
+
+        }
+
+    }
+}
